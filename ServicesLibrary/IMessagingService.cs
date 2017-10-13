b50433b538bb1fc2b34f677eebafd41e89c8cd31
Adedayo Adebiyi <adedayo.adebiyi@memorialhermann.org>
2017-10-13 09:47:11 -0500

    project init

---
+++ ServicesLibrary/IMessagingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLibrary
+{
+    public interface IMessagingService
+    {
+
+        Task<bool> SendTextMessage(string from, string to, string subject, string message);
+        Task<bool> SendMailMessage(string messengerName, string from, string to, string subject, string htmlMessage, string textMessage);
+    }
+}
---
+++ ServicesLibrary/MessagingService.cs
@@ -0,0 +1,64 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace ServicesLibrary
+{
+    public class MessagingService : IMessagingService
+    {
+
+        private SendGridClient _mailClient;
+        private TwilioClient _textClient;
+
+
+        public MessagingService()
+        {
+
+        }
+
+
+        public MessagingService(SendGridClient mailClient, TwilioClient textClient)
+        {
+            _mailClient = mailClient ?? throw new ArgumentNullException();
+            _textClient = textClient ?? throw new ArgumentNullException();
+        }
+
+        public async Task<bool> SendTextMessage(string from, string to, string subject, string textMessage)
+        {
+            var message = await MessageResource.CreateAsync(to: new PhoneNumber(to),from: new PhoneNumber(from),body: textMessage);
+            if (message.Status == MessageResource.StatusEnum.Sent || message.Status == MessageResource.StatusEnum.Sending)               
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> SendMailMessage(string messengerName, string from, string to, string subject, string htmlMessage, string textMessage )
+        {
+            var sender = new EmailAddress(from, messengerName);
+            var mailSubject = subject;
+            var recipient = new EmailAddress(to, null);
+            var plainTextContent = textMessage;
+            var htmlContent = htmlMessage;
+            var msg = MailHelper.CreateSingleEmail(sender, recipient, subject, plainTextContent, htmlContent);
+
+            var response = await _mailClient.SendEmailAsync(msg);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
